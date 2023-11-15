0ecb12a6695ef85c42b299aa3b92d71aa32c3d8e
Swapnil Dhanwal <dhanwal@usc.edu>
2023-11-14 23:07:16 -0800

    Do not consider nothing class while generating train/test files

--- src/data_multiclass.py
+++ src/data_multiclass.py
@@ -1,15 +1,14 @@
 import json
 import os
-from random import sample
-
 from os.path import join
-from typing import Dict, List
+from random import sample
 
 CLASS_SIZE = 500
 
 
 def prepare_class(name: str, _type: str, base_read_path: str, base_write_path: str, index: int):
-    embeddings = []
+    positives = []
+    negatives = []
     for file_name in os.listdir(join(base_read_path, _type)):
         if name not in file_name:
             continue
@@ -19,10 +18,13 @@ def prepare_class(name: str, _type: str, base_read_path: str, base_write_path: s
                 data["name"] = name
                 if data["label"] == 1:
                     data["label"] = index + 1
-                    embeddings.append(data)
-    embeddings = sample(embeddings, k=min(CLASS_SIZE, len(embeddings)))
-    negatives = sample_negatives(_type, index)
-    embeddings.extend(negatives)
+                    positives.append(data)
+                else:
+                    data["label"] = 0
+                    negatives.append(data)
+    positives = sample(positives, k=min(CLASS_SIZE, len(positives)))
+    negatives = sample(negatives, k=min(CLASS_SIZE, len(negatives)))
+    embeddings = positives + negatives
     embeddings = sample(embeddings, k=len(embeddings))
     write_filename = "%s_%d.jsonl" % (_type, index)
     with open(os.path.join(base_write_path, _type, write_filename), "w") as f_write:
@@ -30,26 +32,11 @@ def prepare_class(name: str, _type: str, base_read_path: str, base_write_path: s
             f_write.write(json.dumps(embedding) + "\n")
 
 
-def sample_negatives(_type: str, index: int) -> List[Dict]:
-    if index >= 1:
-        return []
-    path = "../t5p_small_embeddings/%s/%s_%d.jsonl" % (_type, _type, index)
-    embeddings = []
-    with open(path) as f:
-        for line in f.readlines():
-            data = json.loads(line)
-            if data["label"] == 0:
-                data["name"] = "nothing"
-                data["label"] = 0
-                embeddings.append(data)
-    return sample(embeddings, k=min(CLASS_SIZE, len(embeddings)))
-
-
 def main():
     classes = ["dos", "+info", "bypass", "priv", "other"]
     for i, c in enumerate(classes):
-        prepare_class(c, "train", "../multiclass_embeddings/", "../t5p_small_multiclass_embeddings/", i)
-        prepare_class(c, "test", "../multiclass_embeddings/", "../t5p_small_multiclass_embeddings/", i)
+        prepare_class(c, "train", "../multiclass_embeddings/", "../t5p_small_multiclass_multilabel_embeddings/", i)
+        prepare_class(c, "test", "../multiclass_embeddings/", "../t5p_small_multiclass_multilabel_embeddings/", i)
 
 
 if __name__ == '__main__':
